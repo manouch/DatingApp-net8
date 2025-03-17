f41379b1ff8fc023bd1ff997e6219b8494b22d9a
manouch <manoujon@gmail.com>
2025-03-17 11:21:37 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,3 @@
-// using API.Data;
-// using Microsoft.EntityFrameworkCore;
-
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-// builder.Services.AddControllers();
-// builder.Services.AddDbContext<DataContext>(opt => 
-// {
-//   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// });
-
-// var app = builder.Build();
-
-// // Configure the HTTP request pipeline.
-// app.MapControllers();
-
-// app.Run();
-
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,10 +9,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
+
+
+
+
