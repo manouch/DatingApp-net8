53d24c77b3fe570b3995eb764c36055210c09665
manouch <manoujon@gmail.com>
2025-05-22 09:54:49 -0500

    End of section 15

---
+++ API/DTOs/CreateMessageDto.cs
@@ -0,0 +1,10 @@
+using System;
+using AutoMapper.Configuration.Conventions;
+
+namespace API.DTOs;
+
+public class CreateMessageDto
+{
+  public required string RecipientUsername { get; set; }
+  public required string Content { get; set; }
+}
